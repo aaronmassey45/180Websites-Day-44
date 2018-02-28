ed05752673f0f55577ff077fed6b53634077d51c
Aaron <aaronmassey45@gmail.com>
2018-02-28 13:16:15 -0600

    STYLE/ Linted

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
 
 import store from './store';
 import App from './components/App';
 
-ReactDOM.render(
+render(
   <Provider store={store}>
     <App />
-  </Provider>, document.getElementById('root'));
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
