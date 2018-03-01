a4c35f266a2542bad5f9e8b804dd5c866fbd0d9a
Aaron <aaronmassey45@gmail.com>
2018-02-28 23:14:14 -0600

    REFACTOR/ Removed description data. Destructed variables for reusability and changed how image was found

--- src/containers/pokemon.js
+++ src/containers/pokemon.js
@@ -1,30 +1,17 @@
 import React from 'react';
-import {Image, Jumbotron, Grid, Row, Col} from 'react-bootstrap';
+import {Image, Jumbotron, Grid, Row} from 'react-bootstrap';
 
 function Pokemon(props) {
-  //takes object and turns into array entries
-  const data = Object.entries(props.data.types);
-  //maps through the array and returns the pokemons types
-  const types = data.map(type => {
-    return <li key={type[1].type.name}>{type[1].type.name}</li>;
-  });
+  const { name, id } = props.data
   return (
     <Jumbotron className="Pokemon animated flipInX">
       <Grid>
         <Row>
-          <h1>{`${props.data.name.charAt(0).toUpperCase()}${props.data.name.slice(1)}`}</h1>
-          <h3>Poke ID: {props.data.id}</h3>
+          <h1>{`${name.charAt(0).toUpperCase()}${name.slice(1)}`}</h1>
+          <h3>Poke ID: {id}</h3>
         </Row>
         <Row>
-          <Image src={props.data.sprites.front_default} className='animated rotateIn'/>
-        </Row>
-        <Row>
-          <p>{props.description}</p>
-        </Row>
-        <hr/>
-        <Row>
-          <Col xs={6}><p>Types:</p></Col>
-          <Col xs={6}><ul>{types}</ul></Col>
+          <Image src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} className='animated rotateIn'/>
         </Row>
       </Grid>
     </Jumbotron>
