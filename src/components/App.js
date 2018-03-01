4a551dc83807fc6389e98e6464544d140de160ed
Aaron <aaronmassey45@gmail.com>
2018-02-28 23:12:59 -0600

    REFACTOR/ Linted and removed description variable

--- src/components/App.js
+++ src/components/App.js
@@ -3,50 +3,50 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Grid } from 'react-bootstrap';
 
-import {fetchPokemon, fetchSpeciesData} from '../actions'
+import { fetchPokemon } from '../actions';
 import Navbar from '../containers/navbar';
 import SearchBar from './searchbar';
 import Pokemon from '../containers/pokemon';
 import '../App.css';
 
 class App extends Component {
-  componentDidMount() {
-    this.props.fetchPokemon('pikachu');
-    this.props.fetchSpeciesData('pikachu');
-  }
+	componentDidMount() {
+		this.props.fetchPokemon('pikachu');
+	}
 
-  render() {
-    let { data, description, errMessage } = this.props.appState;
+	render() {
+		const { data, errMessage } = this.props.pokeData;
+    console.log(this.props.pokeData);
 
-    return (
-      <div className="App">
-        <Navbar brand="PokeApp!" />
-        <Grid>
-          <SearchBar />
-          {
-            !data || !description ?
-            (
-              errMessage === null  ?
-                <div>
-                  <h2>Loading...</h2>
-                  <i className="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
-                </div> :
-                <h2>{errMessage}</h2>
-            ) :
-            <Pokemon data={data} description={description} />
-          }
-        </Grid>
-      </div>
-    );
-  }
+		return (
+			<div className="App">
+				<Navbar brand="PokeApp!" />
+				<Grid>
+					<SearchBar />
+					{!data ? (
+						!errMessage ? (
+							<div>
+								<h2>Loading...</h2>
+								<i className="fa fa-spinner fa-pulse fa-3x fa-fw" />
+							</div>
+						) : (
+							<h2>{errMessage}</h2>
+						)
+					) : (
+						<Pokemon data={data} />
+					)}
+				</Grid>
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = state => ({
-  appState: state.appState
-})
+	pokeData: state.pokeData,
+});
 
 const mapDispatchToProps = dispatch => {
-  return bindActionCreators({fetchPokemon, fetchSpeciesData}, dispatch)
-}
+	return bindActionCreators({ fetchPokemon }, dispatch);
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
