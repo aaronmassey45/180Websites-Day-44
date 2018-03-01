3cafbf9b718aa66602e91586ebddbf9fc48f13ae
Aaron <aaronmassey45@gmail.com>
2018-02-28 23:07:53 -0600

    REFACTOR/ Linted, removed items pertaining to redux-api-middleware and added redux-promise variable

--- src/store.js
+++ src/store.js
@@ -1,9 +1,9 @@
-import {createStore, applyMiddleware} from 'redux';
-import {apiMiddleware} from 'redux-api-middleware';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
 
 import rootReducer from './reducers';
 
 const INITIAL_STATE = {};
-let store = createStore(rootReducer, INITIAL_STATE, applyMiddleware(apiMiddleware));
+const store = createStore(rootReducer, INITIAL_STATE, applyMiddleware(ReduxPromise));
 
 export default store;
