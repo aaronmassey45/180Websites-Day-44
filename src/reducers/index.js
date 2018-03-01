2b192a5060690a0c85f798070ab1a882fe0b3e3e
Aaron <aaronmassey45@gmail.com>
2018-02-28 23:11:54 -0600

    REFACTOR/ Configured reducer to work with new action and redux-promise. Now uses es6 syntax

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
 import {combineReducers} from 'redux';
-import appState from './reducer_pokemon';
+import pokeData from './reducer_pokemon';
 
 let rootReducer = combineReducers({
-  appState
+  pokeData
 })
 
 export default rootReducer;
--- src/reducers/reducer_pokemon.js
+++ src/reducers/reducer_pokemon.js
@@ -1,38 +1,21 @@
-import {REQUEST, POKEMON_SUCCESS, FAILURE, SPECIES_SUCCESS} from '../actions';
+import { FETCH_POKEMON } from '../actions';
 
 const INITIAL_STATE = {
-  data: null,
-  description: null,
-  errMessage: null
-}
+	data: null,
+	errMessage: null,
+};
 
-export default function appState(state=INITIAL_STATE, action) {
-  switch (action.type) {
-    case REQUEST:
-      return (
-        INITIAL_STATE
-      );
-    case POKEMON_SUCCESS:
-      return (
-        Object.assign({}, state, {
-          data: action.payload,
-          errMessage: null
-        })
-      );
-    case SPECIES_SUCCESS:
-      return (
-        Object.assign({}, state, {
-          description: action.payload.flavor_text_entries[1].flavor_text,
-          errMessage: null
-        })
-      );
-    case FAILURE:
-      return (
-        Object.assign({}, state, {
-          errMessage: "Couldn't find the Pokemon you were looking for. Try another one!",
-        })
-      );
-    default:
-      return state;
-  }
+export default function pokeData(state = INITIAL_STATE, action) {
+	switch (action.type) {
+		case FETCH_POKEMON:
+			return !action.error
+				? { ...state, data: action.payload, errMessage: null }
+				: {
+						...state,
+						data: null,
+						errMessage: 'We could not find the pokemon you are looking for, try again!',
+					};
+		default:
+			return state;
+	}
 }
