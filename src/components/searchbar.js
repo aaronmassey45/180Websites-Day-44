bc14e4d9153d47b0c292a69724a1274b708b8b1e
Aaron <aaronmassey45@gmail.com>
2018-03-01 12:48:17 -0600

    REFACTOR/ Switched to material-ui components Removed state from constructor Doesn't clear input when error occurs

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,47 +1,40 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { TextField, RaisedButton } from 'material-ui';
 
-import {fetchPokemon} from '../actions'
+import { fetchPokemon } from '../actions';
 
 class SearchBar extends Component {
-  constructor(props) {
-    super(props);
+	state = {
+		search: '',
+	};
 
-    this.state = {
-      search: ''
-    }
-  }
+	handleChange = e => {
+		this.setState({ search: e.target.value });
+	};
 
-  handleChange = (e) => {
-    this.setState({ search: e.target.value })
-  }
+	handleSubmit = async e => {
+		e.preventDefault();
+		const pokeData = await this.props.fetchPokemon(this.state.search.toLowerCase());
+		if (pokeData.error) return null;
+		this.setState({ search: '' });
+	};
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.props.fetchPokemon(this.state.search.toLowerCase());
-    // this.props.fetchSpeciesData(this.state.search.toLowerCase());
-    this.setState({ search: '' })
-  }
-
-  render() {
-    return (
-      <div className="SearchBar">
-        <form onSubmit={this.handleSubmit}>
-          <input type="text"
-            onChange={this.handleChange}
-            value={this.state.search}
-            placeholder='Search a pokemon by name or id!'
-          />
-          <button type='submit'>Submit</button>
-        </form>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="SearchBar">
+				<form onSubmit={this.handleSubmit}>
+					<TextField onChange={this.handleChange} value={this.state.search} floatingLabelText="Search a pokemon by name or id!" />
+					<RaisedButton label="Submit" primary={true} type="submit" />
+				</form>
+			</div>
+		);
+	}
 }
 
 const mapDispatchToProps = dispatch => {
-  return bindActionCreators({fetchPokemon}, dispatch)
-}
+	return bindActionCreators({ fetchPokemon }, dispatch);
+};
 
 export default connect(null, mapDispatchToProps)(SearchBar);
