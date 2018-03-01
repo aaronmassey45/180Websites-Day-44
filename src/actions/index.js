255432d6add2e0d44d298055c097cf3bc2f57a18
Aaron <aaronmassey45@gmail.com>
2018-02-28 23:10:29 -0600

    REFACTOR/ Rewrote fetchPokemon action using pokeapi-js-wrapper

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,31 +1,18 @@
-import {CALL_API} from 'redux-api-middleware';
+import * as Pokedex from 'pokeapi-js-wrapper';
 
-const DATA_URL = 'https://pokeapi.co/api/v2/pokemon/';
-const SPECIES_URL = 'https://pokeapi.co/api/v2/pokemon-species/';
+const pokeapi = new Pokedex.Pokedex({
+  protocol: 'https',
+  versionPath: '/api/v2/',
+  cache: true,
+  timeout: 5000
+});
 
-export const REQUEST = 'REQUEST';
-export const FAILURE = 'FAILURE';
-export const POKEMON_SUCCESS = 'POKEMON_SUCCESS';
-export const SPECIES_SUCCESS = 'SPECIES_SUCCESS';
+export const FETCH_POKEMON = 'FETCH_POKEMON';
 
 export function fetchPokemon(pokemon) {
-  const URL = DATA_URL + pokemon;
+  const request = pokeapi.getPokemonSpeciesByName(pokemon);
   return {
-    [CALL_API]: {
-      types:[REQUEST, POKEMON_SUCCESS, FAILURE],
-      endpoint: URL,
-      method: 'GET',
-    }
-  }
-}
-
-export function fetchSpeciesData(pokemon) {
-  const URL = SPECIES_URL + pokemon;
-  return {
-    [CALL_API]: {
-      types:[REQUEST, SPECIES_SUCCESS, FAILURE],
-      endpoint: URL,
-      method: 'GET',
-    }
-  }
+    type: FETCH_POKEMON,
+    payload: request
+  };
 }
